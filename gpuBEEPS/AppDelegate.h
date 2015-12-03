a1a6cf1f5d3940fcc7106b84730e18365145b199
BoJiang <188166614@qq.com>
2015-12-03 10:34:15 +0800

    Initial Commit

---
+++ gpuBEEPS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gpuBEEPS
+//
+//  Created by vk on 15/12/3.
+//  Copyright © 2015年 clover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gpuBEEPS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gpuBEEPS
+//
+//  Created by vk on 15/12/3.
+//  Copyright © 2015年 clover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
